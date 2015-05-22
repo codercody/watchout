0b118c1b074463843cb032cc6b83a632e0ef0325
Cody Scholberg <cscholberg@gmail.com>
2015-05-22 18:20:51 -0400

    :-D

--- watchout.js
+++ watchout.js
@@ -1,2 +1,11 @@
 // start slingin' some d3 here.
+var container         = d3.select("body").append("svg")
+                                    .attr("width", 900)
+                                    .attr("height", 600)
+                                    .style("border", "1px solid black");
 
+var asteroid = d3.selectAll("svg").append("image").attr('xlink:href','asteroid.png')
+              .attr('x="0"')
+              .attr('y="0"')
+              .attr('height="200px"')
+              .attr('width="200px"');
